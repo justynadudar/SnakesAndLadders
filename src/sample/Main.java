05b1dc6540b30ec62dc1e5bb001e50df55acbc1f
Justyna Dudar <justyna.dudar1@gmail.com>
2022-05-01 01:20:55 +0200

    Buttons, Player class, Dice function

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,14 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -14,7 +18,17 @@ public class Main extends Application {
     private static final int tilesInRow = 10;
     private static final int tilesInColumn = 10;
 
+    Player player1;
+    Player player2;
+
+    public boolean gameStart = false;
+    public int rand;
+
+    public int circlePosition[][] = new int[10][10];
+
     private Group tilesGroup = new Group();
+    public Button gameButton;
+    public Label diceResult;
 
     private Parent createContent(){
         Pane root = new Pane();
@@ -31,7 +45,53 @@ public class Main extends Application {
             }
         }
 
+        player1 = new Player(22.5, "player1");
+        player2 = new Player(22.5, "player2");
+
+        Button button1 = new Button("Player1");
+        button1.setTranslateX(500);
+        button1.setTranslateY(180);
+        button1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if(gameStart){
+                    if(player1.isPlayerTurn()){}
+                }
+            }
+        });
+
+        Button button2 = new Button("Player2");
+        button2.setTranslateX(500);
+        button2.setTranslateY(405);
+        button2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if(gameStart){
+                    if(player2.isPlayerTurn()){}
+                }
+            }
+        });
+
+        gameButton = new Button("Start Game");
+        gameButton.setTranslateX(600);
+        gameButton.setTranslateY(25);
+        gameButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                gameButton.setText("Game Started");
+            }
+        });
+
+        diceResult = new Label("0");
+        diceResult.setTranslateX(635);
+        diceResult.setTranslateY(67.5);
+
+        tilesGroup.getChildren().addAll(player1, player2, button1, button2, gameButton, diceResult);
         return root;
+    }
+
+    private void getRandomValue(){
+        rand = (int)(Math.random()*6+1);
     }
 
     @Override
---
+++ src/sample/Player.java
@@ -0,0 +1,33 @@
+package sample;
+
+import javafx.scene.shape.Circle;
+
+public class Player extends Circle {
+    private int playerPosition;
+    private boolean playerTurn;
+    private static double playerXPosition;
+    private static double playerYPosition;
+
+    public Player(double radius, String id){
+        this.playerXPosition = 22.5;
+        this.playerYPosition = 427.5;
+        this.playerPosition = 1;
+        this.playerTurn = true;
+        this.getStyleClass().add("style.css");
+        this.setRadius(radius);
+        this.setId(id);
+        this.setTranslateX(this.playerXPosition);
+        this.setTranslateY(this.playerYPosition);
+    }
+
+    public boolean isPlayerTurn() {
+        return playerTurn;
+    }
+
+    public void changePlayerTurn() {
+        if(this.playerTurn == true)
+            this.playerTurn = false;
+        else
+            this.playerTurn = true;
+    }
+}
