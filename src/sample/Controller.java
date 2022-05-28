12a79d23e968f93031c463fa955194727fa99e6f
TheRealSlimStefan <66061781+TheRealSlimStefan@users.noreply.github.com>
2022-05-28 21:43:36 +0200

    working dice

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,44 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 
 public class Controller {
 
     @FXML
-    void btnAction(ActionEvent event) {
+    private ImageView dice;
+
+    @FXML
+    void exitClicked(ActionEvent event) {
+
+        //TODO POP UP TO ASK "Do you really want to EXIT?"
+
+        Platform.exit();
+        System.exit(0);
+    }
+
+    @FXML
+    void rollClicked(ActionEvent event) {
+        Dice.loadImages();
+        int a = Dice.getRandomValue();
+        switch (a){
+            case 1: dice.setImage(Dice.getDiceImages().get(1));
+            break;
+            case 2: dice.setImage(Dice.getDiceImages().get(2));
+                break;
+            case 3: dice.setImage(Dice.getDiceImages().get(3));
+                break;
+            case 4: dice.setImage(Dice.getDiceImages().get(4));
+                break;
+            case 5: dice.setImage(Dice.getDiceImages().get(5));
+                break;
+            case 6: dice.setImage(Dice.getDiceImages().get(6));
+                break;
+            default:
+                dice.setImage(Dice.getDiceImages().get(0));
+        }
 
     }
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -29,206 +29,203 @@ public class Main extends Application {
     public Button gameButton;
     public Label diceResult;
     public Label gameInfo;
-
-    private int getRandomValue(){
-        return (int)(Math.random() * 6 + 1);
-    }
-
-    private Parent createStartScreen(){
-        Pane root = new Pane();
-        root.setPrefSize(tilesInRow * tileSize + 350, tilesInColumn * tileSize);
-        root.getChildren().addAll(tilesGroup);
-
-        for(int i = 0; i < tilesInRow; i++){
-            for(int j = 0; j < tilesInRow; j++){
-                Tile tile = new Tile(tileSize, tileSize);
-                tile.setTranslateX(tileSize * j);
-                tile.setTranslateY(tileSize * i);
-
-                tilesGroup.getChildren().add(tile);
-            }
-        }
-
-        diceResult = new Label("0");
-        diceResult.setTranslateX(635);
-        diceResult.setTranslateY(67.5);
-
-        gameInfo = new Label("Start a game!");
-        gameInfo.setTranslateX(607.5);
-        gameInfo.setTranslateY(77.5);
-
-        player1 = new Player(22.5, "player1");
-        player2 = new Player(22.5, "player2");
-
-        gameButton = new Button("Start Game");
-        gameButton.setTranslateX(600);
-        gameButton.setTranslateY(25);
-        gameButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if(gameStarted){
-                    gameStarted = false;
-                    exit(0);
-                } else {
-                    gameStarted = true;
-                    System.out.println("Game started!");
-                    gameButton.setText("Exit Game");
-
-                    if((int)(Math.random() * 2 + 1) == 1) {
-                        player1.changePlayerTurn();
-                        gameInfo.setText("Player1 turn!");
-                    }
-                    else {
-                        player2.changePlayerTurn();
-                        gameInfo.setText("Player2 turn!");
-                    }
-                }
-            }
-        });
-
-        Button button1 = new Button("Player 1");
-        button1.setTranslateX(500);
-        button1.setTranslateY(180);
-        button1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if(gameStarted){
-                    if(player1.isPlayerTurn()){
-                        gameInfo.setText("Player2 turn!");
-
-                        int random = getRandomValue();
-
-                        diceResult.setText(String.valueOf(random));
-
-                        player1.changePlayerTurn();
-                        player2.changePlayerTurn();
-                    }
-                }
-            }
-        });
-
-        Button button2 = new Button("Player 2");
-        button2.setTranslateX(500);
-        button2.setTranslateY(405);
-        button2.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if(gameStarted){
-                    if(player2.isPlayerTurn()){
-                        gameInfo.setText("Player1 turn!");
-
-                        int random = getRandomValue();
-
-                        diceResult.setText(String.valueOf(random));
-
-                        player1.changePlayerTurn();
-                        player2.changePlayerTurn();
-                    }
-                }
-            }
-        });
-
-        tilesGroup.getChildren().addAll(player1, player2, button1, button2, gameButton, diceResult, gameInfo);
-        return root;
-    }
-
-    private Parent createContent(){
-        Pane root = new Pane();
-        root.setPrefSize(tilesInRow * tileSize + 350, tilesInColumn * tileSize);
-        root.getChildren().addAll(tilesGroup);
-
-        for(int i = 0; i < tilesInRow; i++){
-            for(int j = 0; j < tilesInRow; j++){
-                Tile tile = new Tile(tileSize, tileSize);
-                tile.setTranslateX(tileSize * j);
-                tile.setTranslateY(tileSize * i);
-
-                tilesGroup.getChildren().add(tile);
-            }
-        }
-
-        diceResult = new Label("0");
-        diceResult.setTranslateX(635);
-        diceResult.setTranslateY(67.5);
-
-        gameInfo = new Label("Start a game!");
-        gameInfo.setTranslateX(607.5);
-        gameInfo.setTranslateY(77.5);
-
-        player1 = new Player(22.5, "player1");
-        player2 = new Player(22.5, "player2");
-
-        gameButton = new Button("Start Game");
-        gameButton.setTranslateX(600);
-        gameButton.setTranslateY(25);
-        gameButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if(gameStarted){
-                    gameStarted = false;
-                    exit(0);
-                } else {
-                    gameStarted = true;
-                    System.out.println("Game started!");
-                    gameButton.setText("Exit Game");
-
-                    if((int)(Math.random() * 2 + 1) == 1) {
-                        player1.changePlayerTurn();
-                        gameInfo.setText("Player1 turn!");
-                    }
-                    else {
-                        player2.changePlayerTurn();
-                        gameInfo.setText("Player2 turn!");
-                    }
-                }
-            }
-        });
-
-        Button button1 = new Button("Player 1");
-        button1.setTranslateX(500);
-        button1.setTranslateY(180);
-        button1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if(gameStarted){
-                    if(player1.isPlayerTurn()){
-                        gameInfo.setText("Player2 turn!");
-
-                        int random = getRandomValue();
-
-                        diceResult.setText(String.valueOf(random));
-
-                        player1.changePlayerTurn();
-                        player2.changePlayerTurn();
-                    }
-                }
-            }
-        });
-
-        Button button2 = new Button("Player 2");
-        button2.setTranslateX(500);
-        button2.setTranslateY(405);
-        button2.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if(gameStarted){
-                    if(player2.isPlayerTurn()){
-                        gameInfo.setText("Player1 turn!");
-
-                        int random = getRandomValue();
-
-                        diceResult.setText(String.valueOf(random));
-
-                        player1.changePlayerTurn();
-                        player2.changePlayerTurn();
-                    }
-                }
-            }
-        });
-
-        tilesGroup.getChildren().addAll(player1, player2, button1, button2, gameButton, diceResult, gameInfo);
-        return root;
-    }
+//
+//
+//    private Parent createStartScreen(){
+//        Pane root = new Pane();
+//        root.setPrefSize(tilesInRow * tileSize + 350, tilesInColumn * tileSize);
+//        root.getChildren().addAll(tilesGroup);
+//
+//        for(int i = 0; i < tilesInRow; i++){
+//            for(int j = 0; j < tilesInRow; j++){
+//                Tile tile = new Tile(tileSize, tileSize);
+//                tile.setTranslateX(tileSize * j);
+//                tile.setTranslateY(tileSize * i);
+//
+//                tilesGroup.getChildren().add(tile);
+//            }
+//        }
+//
+//        diceResult = new Label("0");
+//        diceResult.setTranslateX(635);
+//        diceResult.setTranslateY(67.5);
+//
+//        gameInfo = new Label("Start a game!");
+//        gameInfo.setTranslateX(607.5);
+//        gameInfo.setTranslateY(77.5);
+//
+//        player1 = new Player(22.5, "player1");
+//        player2 = new Player(22.5, "player2");
+//
+//        gameButton = new Button("Start Game");
+//        gameButton.setTranslateX(600);
+//        gameButton.setTranslateY(25);
+//        gameButton.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent actionEvent) {
+//                if(gameStarted){
+//                    gameStarted = false;
+//                    exit(0);
+//                } else {
+//                    gameStarted = true;
+//                    System.out.println("Game started!");
+//                    gameButton.setText("Exit Game");
+//
+//                    if((int)(Math.random() * 2 + 1) == 1) {
+//                        player1.changePlayerTurn();
+//                        gameInfo.setText("Player1 turn!");
+//                    }
+//                    else {
+//                        player2.changePlayerTurn();
+//                        gameInfo.setText("Player2 turn!");
+//                    }
+//                }
+//            }
+//        });
+//
+//        Button button1 = new Button("Player 1");
+//        button1.setTranslateX(500);
+//        button1.setTranslateY(180);
+//        button1.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent actionEvent) {
+//                if(gameStarted){
+//                    if(player1.isPlayerTurn()){
+//                        gameInfo.setText("Player2 turn!");
+//
+//                        int random = getRandomValue();
+//
+//                        diceResult.setText(String.valueOf(random));
+//
+//                        player1.changePlayerTurn();
+//                        player2.changePlayerTurn();
+//                    }
+//                }
+//            }
+//        });
+//
+//        Button button2 = new Button("Player 2");
+//        button2.setTranslateX(500);
+//        button2.setTranslateY(405);
+//        button2.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent actionEvent) {
+//                if(gameStarted){
+//                    if(player2.isPlayerTurn()){
+//                        gameInfo.setText("Player1 turn!");
+//
+//                        int random = getRandomValue();
+//
+//                        diceResult.setText(String.valueOf(random));
+//
+//                        player1.changePlayerTurn();
+//                        player2.changePlayerTurn();
+//                    }
+//                }
+//            }
+//        });
+//
+//        tilesGroup.getChildren().addAll(player1, player2, button1, button2, gameButton, diceResult, gameInfo);
+//        return root;
+//    }
+//
+//    private Parent createContent(){
+//        Pane root = new Pane();
+//        root.setPrefSize(tilesInRow * tileSize + 350, tilesInColumn * tileSize);
+//        root.getChildren().addAll(tilesGroup);
+//
+//        for(int i = 0; i < tilesInRow; i++){
+//            for(int j = 0; j < tilesInRow; j++){
+//                Tile tile = new Tile(tileSize, tileSize);
+//                tile.setTranslateX(tileSize * j);
+//                tile.setTranslateY(tileSize * i);
+//
+//                tilesGroup.getChildren().add(tile);
+//            }
+//        }
+//
+//        diceResult = new Label("0");
+//        diceResult.setTranslateX(635);
+//        diceResult.setTranslateY(67.5);
+//
+//        gameInfo = new Label("Start a game!");
+//        gameInfo.setTranslateX(607.5);
+//        gameInfo.setTranslateY(77.5);
+//
+//        player1 = new Player(22.5, "player1");
+//        player2 = new Player(22.5, "player2");
+//
+//        gameButton = new Button("Start Game");
+//        gameButton.setTranslateX(600);
+//        gameButton.setTranslateY(25);
+//        gameButton.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent actionEvent) {
+//                if(gameStarted){
+//                    gameStarted = false;
+//                    exit(0);
+//                } else {
+//                    gameStarted = true;
+//                    System.out.println("Game started!");
+//                    gameButton.setText("Exit Game");
+//
+//                    if((int)(Math.random() * 2 + 1) == 1) {
+//                        player1.changePlayerTurn();
+//                        gameInfo.setText("Player1 turn!");
+//                    }
+//                    else {
+//                        player2.changePlayerTurn();
+//                        gameInfo.setText("Player2 turn!");
+//                    }
+//                }
+//            }
+//        });
+//
+//        Button button1 = new Button("Player 1");
+//        button1.setTranslateX(500);
+//        button1.setTranslateY(180);
+//        button1.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent actionEvent) {
+//                if(gameStarted){
+//                    if(player1.isPlayerTurn()){
+//                        gameInfo.setText("Player2 turn!");
+//
+//                        int random = getRandomValue();
+//
+//                        diceResult.setText(String.valueOf(random));
+//
+//                        player1.changePlayerTurn();
+//                        player2.changePlayerTurn();
+//                    }
+//                }
+//            }
+//        });
+//
+//        Button button2 = new Button("Player 2");
+//        button2.setTranslateX(500);
+//        button2.setTranslateY(405);
+//        button2.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent actionEvent) {
+//                if(gameStarted){
+//                    if(player2.isPlayerTurn()){
+//                        gameInfo.setText("Player1 turn!");
+//
+//                        int random = getRandomValue();
+//
+//                        diceResult.setText(String.valueOf(random));
+//
+//                        player1.changePlayerTurn();
+//                        player2.changePlayerTurn();
+//                    }
+//                }
+//            }
+//        });
+//
+//        tilesGroup.getChildren().addAll(player1, player2, button1, button2, gameButton, diceResult, gameInfo);
+//        return root;
+//    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -239,7 +236,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.getIcons().add(new Image("/resources/Component 2.png"));
         primaryStage.setTitle("Snakes and Ladders");
-        primaryStage.setScene(new Scene(root, 750, 400));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
